0b4629cf56ed45847fe87b4bedcabe72c8823aad
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-30 12:07:48 +0100

    checkIfMovesAreAvailable - done with if and moveSnake or endGame scen…  … …ario

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -3,130 +3,136 @@ import React from 'react'
 import GameBoard from './GameBoard';
 
 class Snake extends React.Component {
-    constructor(props) {
-        super()
+  constructor(props) {
+    super()
 
-        const halfBoardDimension = Math.ceil(props.boardDimension / 2) - 1
+    const halfBoardDimension = Math.ceil(props.boardDimension / 2) - 1
 
-        this.intervalId = null
-        this.state = {
-            gameBoard: (
-                Array(props.boardDimension)
-                    .fill(
-                        Array(props.boardDimension)
-                            .fill(1)
-                    )
-            ),
-            snakes: [
-                [
-                    { x: halfBoardDimension + 2, y: halfBoardDimension },
-                    { x: halfBoardDimension + 1, y: halfBoardDimension }
-                ],
-                [
-                    { x: halfBoardDimension - 2, y: halfBoardDimension },
-                    { x: halfBoardDimension - 1, y: halfBoardDimension }
-                ]
-            ],
-            directions: [
-                'right',
-                'left'
-            ],
-            meals: [],
-            currentPlayerIndex: 0,
-            gameTickTime: props.startGameTickTime
-        }
+    this.intervalId = null
+    this.currentGameBoard = null
+    this.state = {
+      gameBoard: (
+        Array(props.boardDimension)
+          .fill(
+            Array(props.boardDimension)
+              .fill(1)
+          )
+      ),
+      snakes: [
+        [
+          { x: halfBoardDimension + 2, y: halfBoardDimension },
+          { x: halfBoardDimension + 1, y: halfBoardDimension }
+        ],
+        [
+          { x: halfBoardDimension - 2, y: halfBoardDimension },
+          { x: halfBoardDimension - 1, y: halfBoardDimension }
+        ]
+      ],
+      directions: [
+        'right',
+        'left'
+      ],
+      meals: [],
+      currentPlayerIndex: 0,
+      gameTickTime: props.startGameTickTime
     }
+  }
 
-    componentDidMount() {
-        this.intervalId = setInterval(
-            this.gameTick,
-            this.state.gameTickTime
-        )
-    }
+  componentDidMount() {
+    this.intervalId = setInterval(
+      this.gameTick,
+      this.state.gameTickTime
+    )
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.intervalId)
-    }
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
 
-    gameTick = () => {
-        console.log('tick')
-        this.checkIfMovesAreAvailable()
-    }
+  gameTick = () => {
+    console.log('tick')
+    this.checkIfMovesAreAvailable()
+  }
 
-    checkIfMovesAreAvailable = () => {
-        this.state.snakes.forEach((snakePositions, i) => {
-            const snakeHeadPosition = snakePositions[0]
-            const direction = this.state.directions[i]
-            let newSnakePosition = null
+  checkIfMovesAreAvailable = () => {
+    this.state.snakes.forEach((snakePositions, snakeIndex) => {
+      const snakeHeadPosition = snakePositions[0]
+      const direction = this.state.directions[snakeIndex]
+      let newSnakeHeadPosition = null
 
-            switch (direction) {
-                case 'left':
-                    newSnakePosition = {
-                        x: snakeHeadPosition.x - 1,
-                        y: snakeHeadPosition.y
-                    }
-                case 'right':
-                    newSnakePosition = {
-                        x: snakeHeadPosition.x + 1,
-                        y: snakeHeadPosition.y
-                    }
+      switch (direction) {
+        case 'left':
+          newSnakeHeadPosition = {
+            x: snakeHeadPosition.x - 1,
+            y: snakeHeadPosition.y
+          }
+          break
+        case 'right':
+          newSnakeHeadPosition = {
+            x: snakeHeadPosition.x + 1,
+            y: snakeHeadPosition.y
+          }
+          break
+        case 'top':
+          newSnakeHeadPosition = {
+            x: snakeHeadPosition.x,
+            y: snakeHeadPosition.y - 1
+          }
+          break
+        case 'bottom':
+          newSnakeHeadPosition = {
+            x: snakeHeadPosition.x,
+            y: snakeHeadPosition.y + 1
+          }
+          break
+        default:
+      }
 
+      if (
+        this.currentGameBoard[newSnakeHeadPosition.y] &&
+        this.currentGameBoard[newSnakeHeadPosition.y][newSnakeHeadPosition.x]
+      ) {
+        this.moveSnake(snakeIndex, newSnakeHeadPosition)
+      } else {
+        this.endGame(snakeIndex)
+      }
+    })
+  }
 
-                case 'top':
-                    newSnakePosition = {
-                        x: snakeHeadPosition.x,
-                        y: snakeHeadPosition.y - 1
-                    }
+  composeGameBoard = () => {
+    const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
 
-
-                case 'bottom':
-                    newSnakePosition = {
-                        x: snakeHeadPosition.x,
-                        y: snakeHeadPosition.y + 1
-                    }
-                    break
-                default
-
-
-}
-
-        })
-    }
-
-    composeGameBoard = () => {
-        const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
-
-        this.state.snakes
-            .forEach(snake => (
-                snake.forEach(bodyCellPosition => (
-                    gameBoardCopy[bodyCellPosition.y][bodyCellPosition.x] = 0
-                ))
-            ))
-
-        this.state.meals.forEach(mealPosition => (
-            gameBoardCopy[mealPosition.y][mealPosition.x] = 'F'
+    this.state.snakes
+      .forEach(snake => (
+        snake.forEach(bodyCellPosition => (
+          gameBoardCopy[bodyCellPosition.y][bodyCellPosition.x] = 0
         ))
+      ))
 
-        return gameBoardCopy
-    }
+    this.state.meals.forEach(mealPosition => (
+      gameBoardCopy[mealPosition.y][mealPosition.x] = 'F'
+    ))
 
-    render() {
-        const gameBoard = this.composeGameBoard()
+    return gameBoardCopy
+  }
 
-        return (
-            <div>
-                <GameBoard
-                    gameBoard={gameBoard}
-                />
-            </div>
-        )
-    }
+  render() {
+    this.currentGameBoard = this.composeGameBoard()
+
+    return (
+      <div>
+        <GameBoard
+          gameBoard={this.currentGameBoard}
+        />
+      </div>
+    )
+  }
 }
 
 Snake.defaultProps = {
-    // @TODO it should be checked if bigger than eg. 5
-    boardDimension: 11,
-    startGameTickTime: 1000
+  // @TODO it should be checked if bigger than eg. 5
+  boardDimension: 11,
+  startGameTickTime: 1000
 }
 
 export default Snake
