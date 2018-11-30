3ac750cd1379e51d14c4d01c9a6f5a1f1f91003b
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-30 11:28:55 +0100

    Placing snakes in the middle of board

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -2,54 +2,66 @@ import React from 'react'
 import GameBoard from './GameBoard';
 
 class Snake extends React.Component {
-    state = {
-        gameBoard: (
-            Array(this.props.boardDimension)
-                .fill(
-                    Array(this.props.boardDimension)
-                        .fill(1)
-                )
-        ),
-        snakes: [
-            [
-                { x: 5, y: 5 },
-                { x: 5, y: 6 }
-            ],
-            [
-                { x: 0, y: 0 },
-                { x: 0, y: 1 }
-            ]
+  constructor(props) {
+    super()
+
+    const halfBoardDimension = Math.ceil(props.boardDimension / 2) - 1
+
+    this.state = {
+      gameBoard: (
+        Array(props.boardDimension)
+          .fill(
+            Array(props.boardDimension)
+              .fill(1)
+          )
+      ),
+      snakes: [
+        [
+          { x: halfBoardDimension + 2, y: halfBoardDimension },
+          { x: halfBoardDimension + 1, y: halfBoardDimension }
+        ],
+        [
+          { x: halfBoardDimension - 2, y: halfBoardDimension },
+          { x: halfBoardDimension - 1, y: halfBoardDimension }
         ]
+      ],
+      meals: []
     }
+  }
 
-    composeGameBoard = () => {
-        const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
+  composeGameBoard = () => {
+    const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
 
-        this.state.snakes
-            .forEach(snake => (
-                snake.forEach(bodyCellPosition => (
-                    gameBoardCopy[bodyCellPosition.y][bodyCellPosition.x] = 0
-                ))
-            ))
-        return gameBoardCopy
-    }
+    this.state.snakes
+      .forEach(snake => (
+        snake.forEach(bodyCellPosition => (
+          gameBoardCopy[bodyCellPosition.y][bodyCellPosition.x] = 0
+        ))
+      ))
 
-    render() {
-        const gameBoard = this.composeGameBoard()
+    this.state.meals.forEach(mealPosition => (
+      gameBoardCopy[mealPosition.y][mealPosition.x] = 'F'
+    ))
 
-        return (
-            <div>
-                <GameBoard
-                    gameBoard={gameBoard}
-                />
-            </div>
-        )
-    }
+    return gameBoardCopy
+  }
+
+  render() {
+    const gameBoard = this.composeGameBoard()
+
+    return (
+      <div>
+        <GameBoard
+          gameBoard={gameBoard}
+        />
+      </div>
+    )
+  }
 }
 
 Snake.defaultProps = {
-    // @TODO it should be checked if bigger than eg. 5
-    boardDimension: 10
+  // @TODO it should be checked if bigger than eg. 5
+  boardDimension: 11
 }
 
 export default Snake
