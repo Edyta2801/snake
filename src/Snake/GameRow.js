4bd524fb8214149b2b030ba6683711926372c430
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-30 10:36:03 +0100

    Styling components - gameoard with square cells

--- src/Snake/GameRow.js
+++ src/Snake/GameRow.js
@@ -9,18 +9,26 @@ const style = {
 
 
 
-const GameRow = (props) => (
-    <div
-        style={style}
-    >
-        {
-            props.row.map(cell => (
-                <GameCell
-                    cell={cell}
-                    numberOfCells={props.row.length}
-                />
-            ))
-        }
-    </div>
-)
+const GameRow = (props) => {
+    const numberOfCells = props.row.length
+
+
+    return (
+        < div
+            style={{
+                ...style,
+                height: 100 / numberOfCells + '%'
+            }}
+        >
+            {
+                props.row.map(cell => (
+                    <GameCell
+                        cell={cell}
+                        numberOfCells={props.row.length}
+                    />
+                ))
+            }
+        </div >
+    )
+}
 export default GameRow 
