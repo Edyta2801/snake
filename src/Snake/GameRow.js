3734ad4129f86d5be0fb811e0d0355e51a5f41f3
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-30 10:19:42 +0100

    pass props.row.length to cell

--- src/Snake/GameRow.js
+++ src/Snake/GameRow.js
@@ -17,6 +17,7 @@ const GameRow = (props) => (
             props.row.map(cell => (
                 <GameCell
                     cell={cell}
+                    numberOfCells={props.row.length}
                 />
             ))
         }
