d55c5b535848c42908bea2866b76bd3c8709035d
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-30 09:48:14 +0100

    Rendering gameBoard array

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -1,32 +1,45 @@
 import React from 'react'
 
 class Snake extends React.Component {
-  state={
-      gameBoard:Array(this.props.boardDimension)
-      .fill(
-          Array(this.props.boardDimension)
-          .fill(1)
-      )
-  }
-  
-  
-  
-  
-  
-  
-  
-    render(){
-    return(
-      <div>
-        Snake
-      </div>
-    )
-  }
+    state = {
+        gameBoard: Array(this.props.boardDimension)
+            .fill(
+                Array(this.props.boardDimension)
+                    .fill(1)
+            )
+    }
+
+
+
+
+
+
+
+    render() {
+        const gameBoard = JSON.parse(JSON.stringify(this.state.gameBoard))
+        return (
+            <div>
+                {
+                    gameBoard.map(row => (
+                        <div>
+                            {
+                                row.map(cell =>(
+                                    <div>
+                                        {cell}
+                                    </div>
+                                ))
+                            }
+                    </div>
+                    ))
+                }
+            </div>
+        )
+    }
 }
 
-Snake.defaultProps={
+Snake.defaultProps = {
     //@TODO it should be checked if bigger than eg. 5
-    boardDimension:10
+    boardDimension: 10
 }
 
 
