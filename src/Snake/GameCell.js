dbe372be83e023c035fab60cb30caf966ee8d589
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-30 10:16:07 +0100

    add style to cell

--- src/Snake/GameCell.js
+++ src/Snake/GameCell.js
@@ -1,7 +1,18 @@
 import React from 'react'
- const GameCell = (props) => (
-  <div>
-    {props.cell}
-  </div>
+
+const style = {
+    flexBasis: '10%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+
+const GameCell = (props) => (
+    <div
+        style={style}
+    >
+        {props.cell}
+    </div>
 )
- export default GameCell 
+export default GameCell 
