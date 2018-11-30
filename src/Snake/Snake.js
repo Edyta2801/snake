8e4d3163a94610eb99bd2cf9b9bb39c7dea22762
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-30 11:13:01 +0100

    composeGameBoard - displaying multiple snakes

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -2,32 +2,54 @@ import React from 'react'
 import GameBoard from './GameBoard';
 
 class Snake extends React.Component {
-  state = {
-    gameBoard: (
-      Array(this.props.boardDimension)
-        .fill(
-          Array(this.props.boardDimension)
-            .fill(1)
+    state = {
+        gameBoard: (
+            Array(this.props.boardDimension)
+                .fill(
+                    Array(this.props.boardDimension)
+                        .fill(1)
+                )
+        ),
+        snakes: [
+            [
+                { x: 5, y: 5 },
+                { x: 5, y: 6 }
+            ],
+            [
+                { x: 0, y: 0 },
+                { x: 0, y: 1 }
+            ]
+        ]
+    }
+
+    composeGameBoard = () => {
+        const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
+
+        this.state.snakes
+            .forEach(snake => (
+                snake.forEach(bodyCellPosition => (
+                    gameBoardCopy[bodyCellPosition.y][bodyCellPosition.x] = 0
+                ))
+            ))
+        return gameBoardCopy
+    }
+
+    render() {
+        const gameBoard = this.composeGameBoard()
+
+        return (
+            <div>
+                <GameBoard
+                    gameBoard={gameBoard}
+                />
+            </div>
         )
-    )
-  }
-
-  render() {
-    const gameBoard = JSON.parse(JSON.stringify(this.state.gameBoard))
-
-    return (
-      <div>
-        <GameBoard
-          gameBoard={gameBoard}
-        />
-      </div>
-    )
-  }
+    }
 }
 
 Snake.defaultProps = {
-  // @TODO it should be checked if bigger than eg. 5
-  boardDimension: 10
+    // @TODO it should be checked if bigger than eg. 5
+    boardDimension: 10
 }
 
 export default Snake
